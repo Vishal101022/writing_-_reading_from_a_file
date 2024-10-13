fa24259456032f749ab36c669e565c5176d86049
Vishal101022 <raivishal939@gmail.com>
2024-10-13 23:35:21 +0530

    clean up

--- main.js
+++ main.js
@@ -1,41 +1,7 @@
 const http = require("http");
-const fs = require("fs");
+const route = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/") {
-    let readMessage = undefined;
-    if (fs.existsSync("message.txt")) {
-      readMessage = fs.readFileSync("message.txt", "utf-8");
-    }
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title><head>");
-    res.write(
-      `<body><p>${readMessage}</p><form action ="/message" method="post"><input type="text" name="message"><button type="submit">Send</button></form></body>`
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    console.log("in post");
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-});
+const server = http.createServer(route);
 
 server.listen(4000, () => {
   console.log("Listening on port 4000");
