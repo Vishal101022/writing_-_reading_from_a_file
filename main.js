98441d4f98a29ed9a308185ff60c237676752a40
Vishal101022 <raivishal939@gmail.com>
2024-10-15 09:58:27 +0530

    add route

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from express</h1>");
-});
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"placeholder="Product"><input type="text" name="size" placeholder="Product Size"><button type="submit">Add Product</button></form>'
+  );
+})
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from express</h1>');
+})
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
