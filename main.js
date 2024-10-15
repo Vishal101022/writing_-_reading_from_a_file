d4121b739fd7c1dd699c861d3f66302cfd0d5b8f
Vishal101022 <raivishal939@gmail.com>
2024-10-15 11:32:46 +0530

    refactor

--- main.js
+++ main.js
@@ -2,22 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"placeholder="Product"><input type="text" name="size" placeholder="Product Size"><button type="submit">Add Product</button></form>'
-  );
-})
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-})
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from express</h1>');
-})
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
