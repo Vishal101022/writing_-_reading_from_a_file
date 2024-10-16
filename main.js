9865b6377e4eee7a24fb7cc31973f710fe1f32aa
Vishal101022 <raivishal939@gmail.com>
2024-10-16 19:45:13 +0530

    Add middleware

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const contactRoutes = require("./routes/contact");
+const successRoutes = require("./routes/success");
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(contactRoutes);
+app.use(successRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "error404.html"));
 });
 
 app.listen(3000, () => {
